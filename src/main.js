1c1a2b9798d0759dc18f661d1ae0d77faf6364ee
tmddus00227 <yeonee0227@naver.com>
2024-06-26 01:55:48 +0900

    JS-ArrowUp

--- src/main.js
+++ src/main.js
@@ -12,6 +12,18 @@ document.addEventListener("scroll", () => {
   }
 });
 
+// Arrow up버튼을 아래로 스크롤시 투명하게 처리함
+const home = document.querySelector("#home");
+const homeHeight = home.offsetHeight;
+const arrowUp = document.querySelector(".arrow-up");
+document.addEventListener("scroll", () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.style.opacity = 1;
+  } else {
+    arrowUp.style.opacity = 0;
+  }
+});
+
 // Navbar 토글버튼 클릭 처리
 const navbarMenu = document.querySelector(".header__menu");
 const navbarToggle = document.querySelector(".header__toggle");
